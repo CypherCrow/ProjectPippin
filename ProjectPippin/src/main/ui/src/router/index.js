d70bddb0c08f73c78dacc020251efe44deac5d11
Jonah Morgan <jonah3254@gmail.com>
2021-07-01 19:57:10 -0700

    Configured Vue.js to run on this project

--- ProjectPippin/src/main/ui/src/router/index.js
+++ ProjectPippin/src/main/ui/src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+/* import { createRouter, createWebHistory } from 'vue-router'
 
 import About from '../views/About.vue'
 
 /*
     NOTE: Use <router-link to=""></router-link> instead of <a href=""></a> 
-*/
+
 
 const routes = [
     {
@@ -17,4 +17,4 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
     routes,
-})
+}); */
