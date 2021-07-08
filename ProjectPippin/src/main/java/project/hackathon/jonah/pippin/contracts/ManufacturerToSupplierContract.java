e8dd70a62fb6bda4f5d050336bf4a3782cdac5c7
Jonah Morgan <jonah3254@gmail.com>
2021-07-08 03:31:35 -0700

    updated ManufacturerToSupplierContract class
    Added StorageMaps to parameters of the contract.

--- ProjectPippin/src/main/java/project/hackathon/jonah/pippin/contracts/ManufacturerToSupplierContract.java
+++ ProjectPippin/src/main/java/project/hackathon/jonah/pippin/contracts/ManufacturerToSupplierContract.java
@@ -1,9 +1,12 @@
 package project.hackathon.jonah.pippin.contracts; 
 
 import io.neow3j.devpack.annotations.ManifestExtra;
+import io.neow3j.devpack.Hash160;
 import io.neow3j.devpack.Storage;
 import io.neow3j.devpack.StorageContext;
 import io.neow3j.devpack.StorageMap; 
+import io.neow3j.devpack.annotations.Safe;
+import io.neow3j.devpack.annotations.OnDeployment;
 
 @ManifestExtra(key = "name", value="ManufacturerToSupplierContract")
 @ManifestExtra(key = "author", value="Pippin Project")
@@ -11,5 +14,20 @@ public class ManufacturerToSupplierContract {
     
     static StorageContext ctx = Storage.getStorageContext();
     static final StorageMap contractMap = ctx.createMap((byte) 1); 
+    static final StorageMap certificateOriginMap = ctx.createMap((byte) 2);
+    static final StorageMap batchNumberMap = ctx.createMap((byte) 3); 
+    static final StorageMap processingDataMap = ctx.createMap((byte) 4); 
+    static final StorageMap barcodeMap = ctx.createMap((byte) 5); 
+    static final StorageMap shipmentDateMap = ctx.createMap((byte) 6);
+
+    @OnDeployment
+    public static void deploy(boolean update) throws Exception {
+
+    }
+
+    @Safe
+    public static Hash160 getOwner(){
+        return new Hash160(contractMap.get(OWNER_KEY)); 
+    }
 
 }
